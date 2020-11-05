4e91f04a84a2f8da28e77a9ee068b4386d420203
koceww <koceww@gmail.com>
2020-11-05 16:58:20 +0200

    Add Service

--- Data/AirportSystem.Data/Migrations/20201105145736_AddServices.Designer.cs
+++ Data/AirportSystem.Data/Migrations/20201105145736_AddServices.Designer.cs
@@ -4,14 +4,16 @@ using AirportSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace YourProjectName.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201105145736_AddServices")]
+    partial class AddServices
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
